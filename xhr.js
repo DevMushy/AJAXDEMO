c6c01352f383f11ed59508a557d98e5f6326a049
devmushy <ragusamirko@gmail.com>
2023-01-16 17:41:32 +0100

    all complete

--- xhr.js
+++ xhr.js
@@ -1,11 +1,27 @@
-var id = document.getElementById('id')
-id.addEventListener('change', function(){
+var button = document.getElementById("generate");
+var foto = document.getElementById("img");
+var nome = document.getElementById("nome");
+var cognome = document.getElementById("cognome");
+var email = document.getElementById("email");
+
+function generateUser() {
     var xhr = new XMLHttpRequest()
-    xhr.open("GET", 'https://jsonplaceholder.typicode.com/posts/' + id.value)
-    xhr.send()
-    xhr.onload = function(){
-    var text = xhr.response
-    var obj = JSON.parse(text)
-    document.getElementById('data').innerHTML = obj.body;   
-    }   
-})
+    xhr.open("GET", 'https://randomuser.me/api/?gender=female');
+    xhr.send();
+    xhr.onload = function () {
+        var text = xhr.response
+        var obj = JSON.parse(text);
+        var user = obj.results[0];
+        displayUser(user);
+    }
+}
+
+function displayUser(user){
+    foto.src = user.picture.medium
+    nome.innerHTML = user.name.first;
+    cognome.innerHTML = user.name.last;
+    email.innerHTML = user.email;
+}
+
+
+button.addEventListener('click', generateUser);
